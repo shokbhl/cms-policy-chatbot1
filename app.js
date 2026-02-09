267585e8036f155e8d5da83b9fad68a0965ce3d9
shokbhl <shokbhl@gmail.com>
2026-02-09 14:30:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const MENU_ITEMS = {
     { id: "fire_safety", label: "fire safety" },
     { id: "emergency_management", label: "emergency management" },
     { id: "criminal_reference_vsc_policy", label: "criminal reference & policy" },
-    { "id": "health_policy", "title": "Health, Safety & Infection Prevention", "kv_key": "health_policy" }
+    { id: "health_policy", label: "Health & Infection Prevention" },
  
   ],
 
